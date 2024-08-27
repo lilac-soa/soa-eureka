1566379fa73fe6de8b64415af3ad832ddcdf975f
helloJosh <kimjoshua135@gmail.com>
2024-08-28 00:07:11 +0900

    feat: first commit

---
+++ src/main/java/com/lilac/soaeureka/SoaEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.lilac.soaeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaEurekaApplication.class, args);
+    }
+
+}
